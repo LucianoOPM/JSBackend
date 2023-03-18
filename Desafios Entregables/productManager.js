4726e9f0753d860c11ae7e9d63bc71689649ce28
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-03-17 18:25:20 -0600

    Arreglo de un bug

--- Desafios Entregables/productManager.js
+++ Desafios Entregables/productManager.js
@@ -33,7 +33,7 @@ class ProductManager {
                     }
                 }
             })
-            if (count > 1) {
+            if (count > 5) {
                 pushStatus = "Error, el producto repite ciertos campos"
             } else {
                 const id = ProductManager.idProduct += 1
@@ -52,7 +52,7 @@ let producto = new ProductManager()
 console.log(producto.addProduct("Ryzen 5 3600", "Procesador Ryzen 5 3600", 2500, "Sin imagen", "RYZ-5-3600", 15))
 console.log(producto.addProduct("NVIDIA 1660s", "Tarjeta de vídeo marca MSI", 4500, "Sin imagen", "MSI-NVIDIA-1660S", 10))
 console.log(producto.addProduct("SEAGATE 1000GB", "Disco duro de 1TB 7200RPM", 600, "hdd-seagate.jpg", "SEAGATE-1000", 15))
-console.log(producto.addProduct("Ryzen 5 3600", "Procesador Ryzen 5 3600", 2500, "Sin imagen", "RYZ-5-3600", 15))
+console.log(producto.addProduct("Ryzen 7 3700", "Procesador Ryzen 7", 2500, "Sin imagen", "RYZ-7-3700", 15))
 console.log(producto.getProducts())
-console.log(producto.getProductsById(3))
-console.log(producto.getProductsById(4))
+// console.log(producto.getProductsById(3))
+// console.log(producto.getProductsById(4))
