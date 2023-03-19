9035b7927bc3dc6e9b680968be913af67a0acac9
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-03-19 12:45:25 -0600

    Modificaciones realizadas

--- Desafios Entregables/productManager.js
+++ Desafios Entregables/productManager.js
@@ -1,58 +1,45 @@
 class ProductManager {
     constructor(products = []) {
         this.products = products
+        this.idProduct = 0
     }
     getProducts() {
         return this.products
     }
-    getProductsById(idProduct) {
-        let arrProducts = this.products
-        const find = arrProducts.find(value => value.id === idProduct)
+    getProductById(idProduct) {
+        const find = this.products.find(value => value.id === idProduct)
 
         return find !== undefined ? find : "No se encontró un producto con el ID proporcionado"
     }
 
-    static idProduct = 0
-
     addProduct(title, description, price, thumbnail, code, stock) {
-        let newProduct = { title, description, price, thumbnail, code, stock }
-        let productValues = Object.values(newProduct)
+        let newProduct = { title, description, price, thumbnail, code, stock }//Se guarda el nuevo objeto a validar
+        let productValues = Object.values(newProduct) //Se le extraen los valores al nuevo objeto
 
-        let validateValues = productValues.every(value => value.length !== 0 && value !== " ");
+        let validateValues = productValues.every(value => value.length !== 0 && value !== " ");//Se valida que el nuevo objeto no contenga espacios vacios o espacios en blanco
+        let pushStatus = "" //Variable donde se guardará el mensaje de retorno si el producto fue exitosamente ingresado o no
 
-        let pushStatus = ""
-
-        if (validateValues) {
-            let products = this.products
-            let count = 0
-            products.forEach((product) => {
-                let insertedValues = Object.values(product)
-                for (let i = 0; i < insertedValues.length; i += 1) {
-                    if (insertedValues[i] === productValues[i]) {
-                        count += 1
-                    }
-                }
-            })
-            if (count > 5) {
-                pushStatus = "Error, el producto repite ciertos campos"
-            } else {
-                const id = ProductManager.idProduct += 1
-                newProduct.id = id
-                products.push(newProduct)
-                pushStatus = "El producto se agregó correctamente"
+        if (validateValues) {//Si la validación de los campos vacios es true hace lo siguiente
+            let repetedCode = this.products.every(product => product.code.toLowerCase() !== newProduct.code.toLowerCase())//Se valida que el "CODE" de los nuevos productos, no se repita
+            if (repetedCode) {//Si el código no se repite, pushea el objeto al array y entrega un mensaje de aprobación
+                newProduct.id = this.idProduct += 1//Genera un ID autoincrementable
+                this.products.push(newProduct)
+                pushStatus = "El producto se ingresó correctamente"
+            } else {//Si el código se repite, entrega un código de desaprobación
+                pushStatus = "El producto repite su código, favor de verificarlo"
             }
-        } else {
+        } else {//Si validatevalues es false, hace esto
             pushStatus = "Por favor verifique que los valores no estén vacíos"
         }
-        return pushStatus
+        return pushStatus//Valor de retorno según la agregación del nuevo objeto
     }
 }
 
 let producto = new ProductManager()
+console.log(producto.getProducts())
 console.log(producto.addProduct("Ryzen 5 3600", "Procesador Ryzen 5 3600", 2500, "Sin imagen", "RYZ-5-3600", 15))
 console.log(producto.addProduct("NVIDIA 1660s", "Tarjeta de vídeo marca MSI", 4500, "Sin imagen", "MSI-NVIDIA-1660S", 10))
-console.log(producto.addProduct("SEAGATE 1000GB", "Disco duro de 1TB 7200RPM", 600, "hdd-seagate.jpg", "SEAGATE-1000", 15))
+console.log(producto.addProduct("SEAGATE 1000GB", "Disco duro de 1TB 7200RPM", 600, "hdd-seagate.jpg", "SEAGATE-1000GB", 15))
 console.log(producto.addProduct("Ryzen 7 3700", "Procesador Ryzen 7", 2500, "Sin imagen", "RYZ-7-3700", 15))
 console.log(producto.getProducts())
-// console.log(producto.getProductsById(3))
-// console.log(producto.getProductsById(4))
+//console.log(producto.getProductById(5))
