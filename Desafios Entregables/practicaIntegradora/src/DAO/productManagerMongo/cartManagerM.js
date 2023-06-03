5030fb02fa09391ca20205dcbc2378097eac00c1
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-06-02 22:03:23 -0600

    Actualizado el manager de carts

--- Desafios Entregables/practicaIntegradora/src/DAO/productManagerMongo/cartManagerM.js
+++ Desafios Entregables/practicaIntegradora/src/DAO/productManagerMongo/cartManagerM.js
@@ -1,106 +1,119 @@
 const { cartModel } = require('../models/cartModel.js')
 
 class CartManagerM {
-    newCart = async () => {
+    newCart = async (cart) => {
         try {
-            return await cartModel.create({
-                products: []
-            })
+            //cart.user = "646d5a8109fa4a0a92c3e1d3" El user es opcional pero si se quiere colocar un user dentro del carrito descomentar esta linea
+            //Crea un carrito nuevo con un array de productos vacios (de momento sin un usuario vinculado)
+            return await cartModel.create(cart)
         } catch (error) {
             return `ERROR: ${error}`
         }
-        //Crea un carrito nuevo con un array de productos vacios (de momento sin un usuario vinculado)
     }
 
     getCarts = async (params) => {
         try {
-            return cartModel.paginate({ _id: params.cid }, {
+            const cart = await cartModel.paginate({ _id: params.cid }, {
                 lean: true,
-                populate: 'products.idProduct'
+                populate: 'products.product'
             })
+            if (!cart) throw new Error('El carrito no existe')
+            return cart
         } catch (error) {
-            return `ERROR: ${error}`
+            throw error
         }
         //Retorna los carritos según la query que se requiera, ya sea limit, cid y si no es ninguna de las dos, por defecto trae todos los carritos
     }
 
-    addProductInCart = async (body, params) => {
+    incProducts = async (body, params) => {//Crear este
         try {
-            //Inserts recibe un array de productos llamado inserts con formato
-            const { inserts, qty } = body
+            const { cid, pid } = params
+            const test = await cartModel.findById(cid)
+
+            if (!test) throw new Error('No se encontró el carrito de compras especificado')
+
+            test.products.forEach(prod => {
+                if (prod.product.toString() === pid) {
+                    prod.qty += body.qty
+                }
+            })
+            return test.save()
+        } catch (error) {
+            throw error.message
+        }
+    }
+
+    addProductInCart = async (body, params) => {//hacer un método de inserción de objetos y otro método de actualización.
+        try {
+            /*---------------------------------------------------------------------
+            Recibe por body un array de productos con el siguiente formato:
+            [
+                {"product": "645894b68e1329db2f521e21"},
+                {"product":"6457285a85ea19bcda167afd"},
+                {"product": "645acbe08953e77be82ce17e"}
+            ]
+            Y por defecto siempre tienen la cantidad de 1 a menos de que dentro del array tengan otro valor en la propiedad "qty"
+            --------------------------------------------------------------------------------
+            */
+            //Si no trae productos, retorna un error
+            const bodyKeys = Object.keys(body)
+            const bodyValues = Object.values(body)
+            if (bodyKeys.length === 0 || bodyValues.length === 0) throw new Error(`No hay productos que ingresar`)
+            //recorrer el array de productos insertados, si un ID del producto coincide con un producto de "cartsExists", aumentar en 1 la cantidad, si no existe, lo pusheas
 
             //cid es el req.params que recibe el id del carrito
-            const { cid, pid } = params
+            const { cid } = params
 
             //Revisa si el carrito existe y lo trae, si no, retorna un error
             const cartExists = await cartModel.findOne({ _id: cid })
 
-            //Si existe, actualizar su cantidad
+            //Si el carrito no existe, retorna el error
+            if (!cartExists) throw new Error(cartExists)
 
-            //Si no trae productos, retorna un error
-            if (!(inserts || qty)) {
-                throw new Error(`No hay productos que ingresar`)
-            }
+            //Si el carrito está vacio, le pusheamos todo lo que contenga en body
+            if (cartExists.products.length === 0) return await cartModel.updateOne({ _id: cid }, { $push: { products: body } })
 
-            //Si el carrito no tiene productos
-            if (cartExists.products.length === 0) {
-                //Si no tenemos pid y no tenemos quantity que son params para actualizar unicamente la cantidad, pushea el carrito
-                if (!pid && !qty) {
-                    return await cartModel.updateOne({ _id: cid }, { $push: { products: inserts } })
+            //Si no está vacio, recorremos sus elementos y si encuentra similitudes, aumenta en 1 su cantidad, si no encuentra similitudes, pushea el producto.
+            body.forEach(element => {
+                const finded = cartExists.products.findIndex(prod => prod.product.toString() === element.product)
+                if (finded !== -1) {
+                    cartExists.products[finded].qty += 1
+                } else {
+                    cartExists.products.push(element)
                 }
-                //Si tenemos pid y tenemos quantity, retorna error, ya que no se puede actualizar la cantidad si no hay productos en el carrito
-                throw new Error(`El carrito de compras está vacio`)
-            }
-            //Si params y body tienen las propiedades PID y QTY actualizan la cantidad de productos pasados por body en el producto params
-            if (pid && qty) {
-                return await cartModel.findOneAndUpdate({ _id: cid, "products.idProduct": pid }, { $inc: { "products.$.qty": qty } }, { new: true })
-            }
-            //Le di muchas vueltas intentando hacerlo con mongo, al final lo hice con JS, si el carrito de mongo YA tiene el ID del carrito pasado por req.body, suma la cantidad otorgada
-            const update = cartExists.products.map(newProduct => {
-                const updateValues = inserts.find(foo => foo.idProduct === newProduct.idProduct.toString())
-                return updateValues ? { "idProduct": updateValues.idProduct, "qty": updateValues.qty + newProduct.qty } : newProduct
-            })
-
-            //Si no lo encuentra, agrega.
-            const insert = inserts.filter(test => !cartExists.products.find(values => values.idProduct.toString() === test.idProduct))
-            update.push(...insert)
-
-            //Actualizamos el carrito de mongo según el nuevo array
-            return cartModel.findOneAndUpdate({ _id: cid }, { $set: { products: update } }, { new: true })
+            });
+            //Y se guarda el producto ya actualizado.
+            cartExists.save()
+            return cartExists
         } catch (error) {
-            return `ERROR: ${error}`
+            throw error
         }
     }
-    //Si el carro buscado con el ID no tiene producto con el IDproducto proporcionado, ingresar 1, si ya tiene un producto con el ID proporcionado sumar 1 a la cantidad establecida
 
-    delCart = async (params) => {
+    clearCart = async (param) => {
         try {
-            //Destructuring de los params recibidos por req.params
-            const { cid, pid } = params
+            const { cid } = param
+            const cart = await cartModel.findById(cid)
 
-            //destructuring del array del carrito
-            //const cart = await cartModel.find({ _id: cid }) Si no funciona, descomentar esto lo arregla
-            const cart = await cartModel.findOne({ _id: cid })
+            if (!cart) throw new Error('No se encontró el carrito')
 
-            if (pid) {
-                //Localización de la posición del producto dentro del carrito
-                //const indexProduct = cart[0].products.findIndex(test => test.idProduct.toString() === pid)Si no funciona, descomentar esto lo arregla
-                const indexProduct = cart.products.findIndex(test => test.idProduct.toString() === pid)
-
-                //Si no se encuentra el carrito, arroja error
-                if (indexProduct === -1) {
-                    throw new Error(`No se encontró el producto con el ID: ${cid}`)
-                }
-
-                //Actualizar el carrito con el operador $pull el cual remueve de un array las instancias encontradas según la condicion especificada 
-                //https://www.mongodb.com/docs/v5.2/reference/operator/update/pull/
-
-                return await cartModel.updateOne({ _id: cid }, { $pull: { products: { idProduct: pid } } })
-            }
-            //Si la URL no tiene un ID del producto como parametro, se elimina todos los productos del carrito con el ID especificado, conservado aún el carrito.
+            //Actualizar el carrito con el operador $pull el cual remueve de un array las instancias encontradas según la condicion especificada
+            //https://www.mongodb.com/docs/v5.2/reference/operator/update/pull/
             return await cartModel.updateOne({ _id: cid }, { $pull: { products: {} } })
         } catch (error) {
-            return `ERROR: ${error}`
+            throw error.message
+        }
+    }
+    delProduct = async (params) => {
+        try {
+            const { cid, pid } = params
+
+            const cart = await cartModel.findOne({ _id: cid })
+            if (!cart) throw new Error('No se encontró el carrito')
+
+            return await cartModel.updateOne({ _id: cid }, { $pull: { products: { product: pid } } })
+        } catch (error) {
+            throw error.message
         }
     }
 }
