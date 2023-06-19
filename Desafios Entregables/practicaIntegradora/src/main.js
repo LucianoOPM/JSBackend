a159c389e227640427253c1bf3632445a93f0404
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-06-18 19:25:42 -0600

    Actualizado el archivo main.js

--- Desafios Entregables/practicaIntegradora/src/main.js
+++ Desafios Entregables/practicaIntegradora/src/main.js
@@ -2,21 +2,21 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const { Server } = require('socket.io')
-const { create } = require('connect-mongo')
+//const { create } = require('connect-mongo')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
+//const session = require('express-session')
 const main = require('./routers/routes.js')
-const { webSocket } = require('./utils/socketIo.js')
+const { webSocket, chatSocket } = require('./utils/socketIo.js')
 const { connectDB } = require('./config/DBConfig.js')
-const { initPassportMidd, initPassGitHub } = require('./config/passportConfig.js')
+//const { initPassportMidd, /* initPassGitHub */ } = require('./config/passportConfig.js')
 const passport = require('passport')
 const { initPassport } = require('./passport-JWT/passport.config.js')
 
 //instanciaciones
 const app = express()
 connectDB()
-initPassportMidd()
-initPassGitHub()
+//initPassportMidd()
+/* initPassGitHub() */
 
 //Configuraciones
 app.use(express.json())
@@ -25,9 +25,14 @@ app.engine('handlebars', hbs.engine())
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars')
 app.use('/static', express.static(`${__dirname}/public`))
-app.use(cookieParser('Mensaje secreto'))
+//SI SE USA JWT, NO USAR SESSION
+app.use(cookieParser("S1gn3d Co0k13"))
+initPassport()
+passport.use(passport.initialize())
+
 //trabajando con JWT session no se utiliza
-app.use(session({
+/* 
+app.use(session({//No usar con JWT
     store: create({
         mongoUrl: 'mongodb://127.0.0.1:27017/ecommerce',
         mongoOptions: {
@@ -38,25 +43,25 @@ app.use(session({
     secret: 'Mensaje secreto 2',
     resave: false,
     saveUninitialized: false
-}))
-//SI SE USA JWT, NO USAR SESSION
-initPassport()
-passport.use(passport.initialize())
-passport.use(passport.session())
-
+})) 
+*/
+//passport.use(passport.session())//No usar con JWT
 //routers
 app.use(main)
 app.use((err, req, res, next) => {
     console.log(err)
-    res.status(500).send("Todo mal")
+    res.status(500).send(err.message)
 })
 
 //launcher del server
-const httpServer = app.listen(8080, (err) => {
+/* const PORT = process.env.PORT */
+//const PORT = 8080
+const httpServer = app.listen(process.env.PORT, (err) => {
     if (err)`ERROR en el servidor ${err}`
-    console.log("Se inició el servidor.")
+    console.log("Se inició el servidor " + process.env.PORT)
 })
 
 //utilización de sockets
 const io = new Server(httpServer)
 webSocket(io)
+//chatSocket(io)
