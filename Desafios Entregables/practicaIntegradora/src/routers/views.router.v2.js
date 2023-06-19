3cb53cd700c7965bc556d1ce79a25bcc07a21126
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-06-18 19:24:14 -0600

    Actualizadas algunas rutas de vistas

--- Desafios Entregables/practicaIntegradora/src/routers/views.router.v2.js
+++ Desafios Entregables/practicaIntegradora/src/routers/views.router.v2.js
@@ -1,4 +1,6 @@
 const ViewsController = require("../controllers/viewsv2.controller");
+const { isLogged } = require("../middleware/auth");
+const passportCall = require("../passport-JWT/passport.call");
 const { RouterClass } = require("./RouterClass");
 
 const views = new ViewsController()
@@ -11,8 +13,8 @@ class ViewsRouter extends RouterClass {
         this.get('/products/:PID', ['PUBLIC'], views.productsById)//Funciona
         /*Views session*/
         /*Login*/
-        this.get('/login', ['PUBLIC'], /*middleware isLoggued*/views.login)//Funciona//Para todos los usuarios, pero si ya está logueado, redirigir a productos
-        this.get('/register', ['PUBLIC'], /*middleware isLoggued*/views.register)//Funciona//Para todos los usuarios, pero si ya está logueado redirigir a productos
+        this.get('/login', ['PUBLIC'], isLogged, views.login)//Funciona//Para todos los usuarios, pero si ya está logueado, redirigir a productos
+        this.get('/register', ['PUBLIC'], isLogged, views.register)//Funciona//Para todos los usuarios, pero si ya está logueado redirigir a productos
         this.get('/restore', ['PUBLIC'], views.restore)//Funciona
         /*Realtimeproducts*/
         this.get('/realtimeproducts', ['PUBLIC'], views.realtimeproducts)//Funciona//Sólo para personal autorizado(admins)
