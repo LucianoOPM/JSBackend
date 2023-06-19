bbc673c264546b68f5e042ece4ac4ac647528271
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2023-06-18 19:22:27 -0600

    Agregado las configuraciones de passport

--- Desafios Entregables/practicaIntegradora/src/passport-JWT/passport.call.js
+++ Desafios Entregables/practicaIntegradora/src/passport-JWT/passport.call.js
@@ -1,11 +1,17 @@
 const passport = require('passport')
 
-const passportCall = (strategy) => {
-    return async (req, res, next) => {
+const passportCall = (strategy) => {//Se ejecuta la función con una estrategia
+    return async (req, res, next) => {//Retorna la ejecución de una función
         passport.authenticate(strategy, (err, user, info) => {
-            const { user: currentUser } = user
-            if (err) return next(err)
+            //Realiza un authenticate que pasa a validar el archivo passport.config.js dependiendo de la estrategia
+            if (err) return next(err) //Si recibe un error, retorna directamente el error
+            const { user: currentUser } = user //Si no, recibimos los datos del usuario por medio de su token
+
+            //Si no hay un usuario, retorna un error
+            //Si no hay un token de autorización, info retorna el error
             if (!currentUser) return res.status(401).send({ status: 'error', error: info.message ? info.message : info.toString() })
+
+            //Si hay un usuario, lo guarda en el req.user
             req.user = currentUser
             next()
         })(req, res, next)
--- Desafios Entregables/practicaIntegradora/src/passport-JWT/passport.config.js
+++ Desafios Entregables/practicaIntegradora/src/passport-JWT/passport.config.js
@@ -1,12 +1,21 @@
 const passport = require('passport')
 const jwt = require('passport-jwt')
+const gitStrategy = require('passport-github2')
+const {
+    SECRET_KEY,
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    GITHUB_CALLBACK_URL
+} = require('../config/config')
 
 const JWTStrategy = jwt.Strategy
 const ExtractJWT = jwt.ExtractJwt
 
-const cookieExtractor = req => {
-    let token = null
+
+const cookieExtractor = req => {//Extraer las cookies al momento de acceder a las vistas
+    let token = null//Si no hay cookies, retorna un null
     if (req && req.cookies) {
+        //Si hay cookies, retorna la cookie
         token = req.cookies['coderCookieToken']
     }
     return token
@@ -16,25 +25,35 @@ const initPassport = () => {
     //Ya que el modulo session a través de es quien controlaba las cookies hay que recordar que si vamos a usar JWT no usaremos session.
     //estrategia JWT
     passport.use('jwt', new JWTStrategy({
+        //Obtiene el codigo jwt de la peticion y ejecuta el método de extraccion de la funcion cookieExtractors
         jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]),
-        secretOrKey: "PalabraJWTSecreta"//Esta palabra debe estar privada desde el .env
+        //Decodea al usuario extraido de la cookie según la palabra con la que se haya codeado, en este caso la palabra es "PalabraJWTSecreta"
+        secretOrKey: SECRET_KEY//Esta palabra debe estar privada desde el .env
     }, async (jwt_payload, done) => {
         try {
+            console.log(jwt_payload);
+            //Si todo lo anterior resulta bien, ejecuta la callback pasando el usuario ya decodificado de vuelta a "passport.call.js"
             return done(null, jwt_payload)
         } catch (error) {
             return done(error)
         }
     }))
 
-    //estraregia current
-    passport.use('current', new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromExtractors([cookieExtractor]),
-        secretOrKey: "PalabraJWTSecreta"
-    }, async (jwt_payload, done) => {
+    passport.use('github', new gitStrategy({
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: GITHUB_CALLBACK_URL
+    }, async (accessToken, refreshToken, profile, done) => {
         try {
-            return done(null, jwt_payload)
+            const { name, email } = profile._json
+            const userGitHub = {
+                user: {
+                    name, email, password: ""
+                }
+            }
+            return done(null, userGitHub)
         } catch (error) {
-            return done(error)
+            if (error) return done(error)
         }
     }))
 }
